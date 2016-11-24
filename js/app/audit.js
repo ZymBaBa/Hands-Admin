253582bcb850ea527bee69d6b374863c2f18e740
ZymBaBa <iformatzjy@hotmail.com>
2016-11-24 08:24:41 +0800

    查看完成
    查看功能完成

--- js/app/audit.js
+++ js/app/audit.js
@@ -1,12 +1,22 @@
 /**
  * Created by iform on 2016-11-23.
  */
+app.filter("jsonDate", function($filter) {
+    return function(input, format) {
+        //先得到时间戳
+        var timestamp = Number(input.replace(/\/Date\((\d+)\)\//, "$1"));
 
-app.controller('audit', ['$scope', '$modal', '$log','$http', function($scope, $modal, $log,$http) {
+        //转成指定格式
+        return $filter("date")(timestamp, format);
+    };
+    //{{"/Date(items.proData)/" | jsonDate:"yyyy-MM-dd HH:mm:ss"}}
+});
+app.controller('audit', ['$scope', '$modal', '$log','$http','$filter', function($scope, $modal, $log,$http,$filter) {
     $http.get("js/app/cart.json").success(function (data) {
         $scope.cart = data.cart;
     });
-    //建立一个产品ID的索引，判断是否ID相等
+
+    //建立一个ID的索引，用来判断是否ID相等
     function findIndex(id) {
         var index = -1;
         angular.forEach($scope.cart, function (item, key) {
@@ -17,23 +27,19 @@ app.controller('audit', ['$scope', '$modal', '$log','$http', function($scope, $m
         });
         return index
     }
-    $scope.tip='张三';
-    $scope.items = ['item1', 'item2', 'item3'];
-    $scope.open = function (size,id) {
-        // var index=findIndex(id);
-        // console.log(index);
-        // if(index!=-1){
-        //     var item=$scope.cart[index];
-        //     console.log(item);
-        //     $scope.tip=item.id;
-        // }
+    //查看
+    $scope.see = function (size,id) {
+        var index=findIndex(id);
+        if(index!=-1){
+            $scope.item=$scope.cart[index];
+        }
         var modalInstance = $modal.open({
             templateUrl: 'tpl/modal/audit.html',
             controller: 'ModalInstanceCtrl',
             size: size,
             resolve: {
                 items: function () {
-                    return [$scope.items,$scope.tip];
+                    return $scope.item;
                 }
             }
         });
@@ -41,19 +47,51 @@ app.controller('audit', ['$scope', '$modal', '$log','$http', function($scope, $m
             $scope.selected = selectedItem;
         });
         modalInstance.opened.then(function () {
-            // console.log($scope.tip);
-            // var index=findIndex(id);
-            // console.log(index);
-            // if(index!=-1){
-            //     var item=$scope.cart[index];
-            //     console.log(item);
-            //     $scope.tip={
-            //         id:item.id
-            //     };
-            // }
-            // console.log($scope.tip);
-        });
 
+        });
+    };
+    //编辑
+    $scope.ext = function (size,id) {
+        var index=findIndex(id);
+        if(index!=-1){
+            $scope.item=$scope.cart[index];
+        }
+        var modalInstance = $modal.open({
+            templateUrl: 'tpl/modal/audit2.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.item;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        });
+        modalInstance.opened.then(function () {
+
+        });
+    };
+    //新增
+    $scope.items=['items'];
+    $scope.add = function (size) {
+        var modalInstance = $modal.open({
+            templateUrl: 'tpl/modal/audit1.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        });
+        modalInstance.opened.then(function () {
+
+        });
     };
 }])
 ;
