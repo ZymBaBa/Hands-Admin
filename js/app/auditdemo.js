9ed6f5d419470aef562f47a6966feea77e8188d7
Zhouym <iformatmm@hotmail.com>
2016-11-27 15:54:54 +0800

    更新了下
    更新了点东西

--- js/app/auditdemo.js
+++ js/app/auditdemo.js
@@ -2,13 +2,12 @@
  * Created by iform on 2016-11-25.
  */
 app.controller('auditdemo',['$scope','$http','$stateParams','$timeout',function ($scope,$http,$stateParams,$timeout) {
-    //拿传过来的ID
     $scope.sum=$stateParams.itemID;
+
+
     $http.get("js/app/cart.json").success(function (data) {
         $scope.cart = data.cart;
     });
-
-    //拿接口里的数据
 
     function findIndex(id) {
         var index = -1;
@@ -29,8 +28,6 @@ app.controller('auditdemo',['$scope','$http','$stateParams','$timeout',function 
             console.log($scope.Data);
         }
     },2000);
-
-
 
     // $scope.items=function () {
     //     var id=Number($scope.sum);
